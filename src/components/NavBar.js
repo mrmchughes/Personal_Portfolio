b9901d74d302d8c6852d902d322b085d7bacbbf0
Michael <mrmchughes27@gmail.com>
2023-10-12 17:11:12 -0400

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+export default function NavBar() {
+    return (
+        <nav>
+            <a href="/">Michael Hughes</a>
+            <div>
+                <a href="/about">About</a>
+                <a href="/tools">Tools</a>
+                <a href="/projects">Projects</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </nav>
+    )
+}
