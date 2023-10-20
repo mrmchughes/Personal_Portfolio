d9ae3bdab6877ae118ee7893d1918957420bb2e5
Michael <mrmchughes27@gmail.com>
2023-10-20 15:02:46 -0400

    sample t ext added to intro and about

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            {/* <p>About me text section will go here</p> */}
+            {/* button with link to view resume at the bottom */}
+            {/* Something like the below */}
+            {/* https://mrmchughes.github.io/assets/MichaelHughes_Resume.pdf */}
+        </div>  
+    )
+}
---
+++ src/components/Intro.js
@@ -0,0 +1,9 @@
+export default function Intro() {
+    return (
+        // Want to include some of those fancy animations for this section when someone first navigates to the page
+        <div>
+            <p>Hi, my name is Michael Hughes.</p>
+            <p>I'm a Full-Stack Engineer</p>
+        </div>
+    )
+}
