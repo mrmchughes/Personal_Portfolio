3ec62d22fcbb678cc9698c92082c8cb834d10a72
Michael <mrmchughes27@gmail.com>
2023-10-24 16:19:16 -0400

    added skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,31 @@
 export default function Skills() {
     return (
         <div>
-            
+            <div>
+                <p>Front End</p>
+                <ul>
+                    <li>React</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Redux</li>
+                </ul>
+            </div>
+            <div>
+                <p>Back End</p>
+                <ul>
+                    <li>Node.js</li>
+                    <li>Postgres</li>
+                    <li>Express.js</li>
+                    <li>MongoDB</li>
+                </ul>
+            </div>
+            <div>
+                <p>Design Tools</p>
+                <ul>
+                    <li>Figma</li>
+                    <li>Excalidraw</li>
+                </ul>
+            </div>
         </div>
     )
 }
