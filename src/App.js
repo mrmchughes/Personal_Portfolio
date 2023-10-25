b6ee55598ed5d740e18e15f426931597ee6092a5
Michael <mrmchughes27@gmail.com>
2023-10-25 15:41:20 -0400

    worked on navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Intro from './components/Intro';
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Intro />
+      <About />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Navbar() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <a href="/">Michael Hughes</a>
+                    </Typography>
+
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <a href="/about">About</a>
+                    </Typography>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <a href="/skills">Skills</a>
+                    </Typography>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <a href="/projects">Projects</a>
+                    </Typography>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <a href="/contact">Contact</a>
+                    </Typography> 
+                </Toolbar>    
+            </AppBar>           
+        </Box>
+    )
+}
+
+// Watch out that the navbar changes when I scroll down the page past the opening section
+// The navbar collapse for a smaller screen
+
+//Use the Responsive App bar with Drawer component so that the navbar collapses for smaller screens
